ce356e890b775140b8988f4b5c7956a1c2ed8691
varodv <varodv@gmail.com>
2021-07-29 22:46:30 +0200

    feat: Vue demo app initial implementation added

---
+++ packages/app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-charts" ref="charts"></div>
+</template>
+
+<script lang="ts">
+  import { init } from 'dv-charts';
+
+  import { defineComponent, onMounted, ref } from 'vue';
+
+  export default defineComponent({
+    name: 'App',
+    setup: () => {
+      const charts = ref();
+
+      onMounted(() => {
+        init(charts.value);
+      });
+
+      return {
+        charts,
+      };
+    },
+  });
+</script>
+
+<style lang="scss">
+  .app-charts {
+    height: 100px;
+  }
+</style>
