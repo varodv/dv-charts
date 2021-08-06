b454d87de0508e1128566e12e9f1d84d66f54a03
varodv <varodv@gmail.com>
2021-08-06 17:45:20 +0200

    feat: Empty ProportionalChartComponent added

---
+++ packages/lib/src/proportional-area-chart/proportional-area-chart.spec.ts
@@ -0,0 +1,7 @@
+import { ProportionalAreaChartComponent } from './proportional-area-chart.component';
+
+describe('ProportionalAreaChartComponent', () => {
+  it('should exist', () => {
+    expect(ProportionalAreaChartComponent).toBeDefined();
+  });
+});
