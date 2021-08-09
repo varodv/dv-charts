6c4839104dfe9ea5b477083a2efe35c728a1f799
varodv <varodv@gmail.com>
2021-08-09 23:23:17 +0200

    feat: lib main file changed to export ProportionalAreaChart

--- packages/lib/src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { init } from './';
-
-describe('init', () => {
-  it('should add dv-charts class to passed element', () => {
-    const element = document.createElement('div');
-    init(element);
-    expect(element.classList.value).toContain('dv-charts');
-  });
-});
