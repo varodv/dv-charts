6b4d7808aee266a03e6eb500ade60716d0cc5e4c
varodv <varodv@gmail.com>
2021-08-09 22:15:29 +0200

    test: AbstractComponent init tests added

--- packages/lib/src/shared/abstract.component.spec.ts
+++ packages/lib/src/shared/abstract.component.spec.ts
@@ -9,12 +9,24 @@ class TestComponent extends AbstractComponent<number[], Config> {
 }
 
 describe('AbstractComponent', () => {
+  const data = [5, 7];
   const defaultConfig: Config = {
     animationsDurationInMillis: 400,
   };
   const config: Config = {
     animationsDurationInMillis: 800,
   };
+
+  const resizeObserverObserveMock = jest.fn();
+  beforeAll(() => {
+    (window as any).ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: resizeObserverObserveMock,
+    }));
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   describe('constructor', () => {
     it('should create a component with the default config', () => {
@@ -24,6 +36,40 @@ describe('AbstractComponent', () => {
 
     it('should create a component with the passed config', () => {
       const component = new TestComponent(config);
+      expect(component['config']).toEqual(config);
+    });
+  });
+
+  describe('init', () => {
+    it('should initialize the component into the passed element', () => {
+      const component = new TestComponent();
+      const element = document.createElement('div');
+      component.init(element);
+      expect(component['element']!.node()).toEqual(element);
+      expect(component['data']).toBeUndefined();
+      expect(component['config']).toEqual(defaultConfig);
+      expect(component['size']).toEqual({ height: 0, width: 0 });
+      expect(resizeObserverObserveMock).toHaveBeenCalledWith(element);
+    });
+
+    it('should initialize the component with the passed data', () => {
+      const component = new TestComponent();
+      component.init(document.createElement('div'), { data });
+      expect(component['data']).toEqual(data);
+      expect(component['config']).toEqual(defaultConfig);
+    });
+
+    it('should initialize the component with the passed config', () => {
+      const component = new TestComponent();
+      component.init(document.createElement('div'), { config });
+      expect(component['data']).toBeUndefined();
+      expect(component['config']).toEqual(config);
+    });
+
+    it('should initialize the component with the passed data and config', () => {
+      const component = new TestComponent();
+      component.init(document.createElement('div'), { data, config });
+      expect(component['data']).toEqual(data);
       expect(component['config']).toEqual(config);
     });
   });
