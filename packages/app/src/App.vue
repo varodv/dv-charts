52726ac5a190534310799954225b4a793baa44fe
varodv <varodv@gmail.com>
2022-05-17 00:04:17 +0200

    feat: App header links for first level routes added

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <app-header />
+  <app-header :routes="ROUTES" />
   <router-view />
 </template>
 
-<script setup>
+<script setup lang="ts">
   import { AppHeader } from './components';
+  import { ROUTES } from './router';
 </script>
 
 <style lang="scss">
--- packages/app/src/components/app-header.component.vue
+++ packages/app/src/components/app-header.component.vue
@@ -1,14 +1,36 @@
 <template>
   <header :class="baseClass">
-    <h1>app</h1>
+    <router-link
+      v-for="{ name } in routes"
+      :key="name"
+      :class="[`${baseClass}__link`, `${baseClass}__link--route`]"
+      :active-class="`${baseClass}__link--active`"
+      :to="{ name }"
+    >
+      {{ name }}
+    </router-link>
   </header>
 </template>
 
-<script setup>
+<script setup lang="ts">
+  import { Route } from '../router';
+
+  const { routes } = defineProps({
+    routes: {
+      type: Array as () => Array<Route>,
+      default: () => [],
+    },
+  });
+
   const baseClass = 'app-header';
 </script>
 
 <style lang="scss" scoped>
   .app-header {
+    &__link {
+      &--active {
+        background-color: yellow;
+      }
+    }
   }
 </style>
