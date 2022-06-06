e5490226734e0219ead604e003446bedbef6fa1a
varodv <varodv@gmail.com>
2022-06-06 20:50:57 +0200

    feat: Pinia added into app

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,11 +1,13 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import App from './App.vue';
-
 import { ROUTER } from './router';
 
 const app = createApp(App);
 
 app.use(ROUTER);
+
+app.use(createPinia());
 
 app.mount('#app');
