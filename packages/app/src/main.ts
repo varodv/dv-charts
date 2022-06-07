c8ec49e4b0c28a937b22a956e7a9f654dd0e821c
varodv <varodv@gmail.com>
2022-06-07 23:58:47 +0200

    feat: Common messages loaded on app init

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -3,11 +3,16 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import { ROUTER } from './router';
+import { useI18n } from './stores';
 
 const app = createApp(App);
 
 app.use(ROUTER);
 
 app.use(createPinia());
+
+const { locale, storeLocaleMessages } = useI18n();
+const { default: messages } = await import(`./i18n/common/${locale}.js`);
+storeLocaleMessages(messages);
 
 app.mount('#app');
