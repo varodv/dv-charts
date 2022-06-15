b1c7e1f7b09c27cb58d17bcc128246e2c7fd83e7
varodv <varodv@gmail.com>
2022-06-15 22:01:55 +0200

    feat: Base component added into lib

---
+++ packages/lib/src/common/base-component.ts
@@ -0,0 +1,69 @@
+import { ComponentConfig, ComponentParams, Size } from './component.types';
+
+export class BaseComponent<DataType, ConfigType extends ComponentConfig> {
+  protected data: DataType | undefined;
+
+  protected config: ConfigType;
+
+  protected size: Size;
+
+  private resizeObserver: ResizeObserver;
+  private resizeObserverInitialized = false;
+
+  protected constructor(protected element: HTMLElement, params?: ComponentParams<DataType, ConfigType>) {
+    this.data = params?.data;
+    const config = this.getDefaultConfig();
+    if (!!params?.config) {
+      Object.assign(config, params.config);
+    }
+    this.config = config;
+
+    const { width, height } = element.getBoundingClientRect();
+    this.size = {
+      width,
+      height,
+    };
+    this.resizeObserver = new ResizeObserver((entries) => {
+      if (!this.resizeObserverInitialized) {
+        this.resizeObserverInitialized = true;
+      } else {
+        const { width, height } = entries.slice(-1)[0]!.contentRect;
+        this.resize({
+          width,
+          height,
+        });
+      }
+    });
+    this.resizeObserver.observe(element);
+  }
+
+  public static create<DataType, ConfigType extends ComponentConfig>(
+    element: HTMLElement,
+    params?: ComponentParams<DataType, ConfigType>,
+  ): Component<DataType, ConfigType> {
+    return new Component(element, params);
+  }
+
+  public update(params: ComponentParams<DataType, ConfigType>): void {
+    if (params.hasOwnProperty('data')) {
+      this.data = params.data;
+    }
+    if (!!params.config) {
+      Object.assign(this.config, params.config);
+    }
+  }
+
+  public destroy(): void {
+    this.resizeObserver.disconnect();
+  }
+
+  protected getDefaultConfig(): ConfigType {
+    return {
+      transitionsDuration: 500,
+    } as ConfigType;
+  }
+
+  protected resize(size: Size): void {
+    this.size = size;
+  }
+}
