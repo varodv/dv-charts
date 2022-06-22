375588e230ed77680d3119f9dc0e2fc4df555549
varodv <varodv@gmail.com>
2022-06-22 22:44:19 +0200

    feat: Empty render implemented for proportional area chart

--- packages/lib/src/proportional-area-chart/proportional-area-chart.ts
+++ packages/lib/src/proportional-area-chart/proportional-area-chart.ts
@@ -1,4 +1,5 @@
 import { scaleLinear, scalePoint } from 'd3-scale';
+import { select, Selection } from 'd3-selection';
 
 import { BaseComponent } from '../common/base-component';
 import { Size } from '../common/component.types';
@@ -11,6 +12,13 @@ import {
 import './proportional-area-chart.scss';
 
 export class ProportionalAreaChart extends BaseComponent<ProportionalAreaChartData, ProportionalAreaChartConfig> {
+  private readonly baseClass = 'dv-proportional-area-chart';
+
+  private readonly selections: {
+    element: Selection<HTMLElement, unknown, any, unknown>;
+    svg: Selection<SVGSVGElement, undefined, HTMLElement, unknown>;
+  };
+
   private readonly scales = {
     position: scalePoint(),
     radius: scaleLinear(),
@@ -19,20 +27,35 @@ export class ProportionalAreaChart extends BaseComponent<ProportionalAreaChartDa
   public constructor(...args: [HTMLElement, ProportionalAreaChartParams]) {
     super(...args);
 
+    const element = select(this.element) as Selection<HTMLElement, unknown, any, unknown>;
+    element.attr('class', this.baseClass);
+    const svg = element.append('svg') as Selection<SVGSVGElement, undefined, HTMLElement, unknown>;
+    svg.style('width', '100%').style('height', '100%');
+    this.selections = {
+      element,
+      svg,
+    };
+
     this.setScalesDomain();
     this.setScalesRange();
+
+    this.render(true);
   }
 
   public update(params: ProportionalAreaChartParams): void {
     super.update(params);
 
     this.setScalesDomain();
+
+    this.render(true);
   }
 
   protected resize(size: Size): void {
     super.resize(size);
 
     this.setScalesRange();
+
+    this.render(false);
   }
 
   private setScalesDomain(): void {
@@ -61,5 +84,9 @@ export class ProportionalAreaChart extends BaseComponent<ProportionalAreaChartDa
     this.scales.position.range([maxRadius, width - maxRadius]);
 
     this.scales.radius.range([0, maxRadius]);
+  }
+
+  private render(animate: boolean): void {
+    console.log('render', this.selections, animate); // TODO: implement method
   }
 }
