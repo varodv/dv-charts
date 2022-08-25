1532a16a93f5195026f893450e0e4c45020c73fe
varodv <varodv@gmail.com>
2022-08-25 20:18:51 +0200

    feat(app): Treemap hovering added

--- packages/app/src/components/treemap/treemap.component.vue
+++ packages/app/src/components/treemap/treemap.component.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-  import { Treemap } from 'dv-charts';
+  import { style, Treemap } from 'dv-charts';
   import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue';
 
   import { useI18n } from '../../stores';
@@ -24,14 +24,12 @@
   const el = ref();
 
   const initialTransitionDelay = (_, index) => index * 100;
+  const mouseTransitionsDuration = 200;
+
+  const hoveredDataItemId = ref(undefined);
 
   const params = computed(() => {
-    const data = props.data.map((dataItem) => ({
-      ...dataItem,
-      style: {
-        strokeWidth: '0',
-      },
-    }));
+    const data = Array.isArray(props.data) ? props.data.map(mapDataItem) : mapDataItem(props.data);
     const config = {
       padding: [30, 5, 5, 5],
       childrenMargin: 5,
@@ -43,10 +41,36 @@
     };
   });
 
+  const mapDataItem = (dataItem, parentStyle?) => {
+    const style = getDataItemStyle(dataItem, parentStyle);
+    const children = dataItem.children?.map((child) => mapDataItem(child, style));
+    return {
+      ...dataItem,
+      ...(!!children && { children }),
+      style,
+    };
+  };
+
+  const getDataItemStyle = (dataItem, parentStyle?) => {
+    return {
+      fill: getDataItemFill(dataItem, parentStyle),
+      ...(!parentStyle && { strokeWidth: '0' }),
+      cursor: 'pointer',
+    };
+  };
+
+  const getDataItemFill = ({ id }, parentStyle?): string => {
+    const { primary, secondary } = style.colors;
+    if (hoveredDataItemId.value === id) {
+      return secondary;
+    }
+    return parentStyle?.fill ?? primary;
+  };
+
   const getContentHtml = (dataItem): string => {
     const text = `${dataItem.id} (${getDataItemValue(dataItem).toLocaleString(locale)})`;
-    return `<div class="${baseClass}__content">
-      <span class="${baseClass}__content-text" title="${text}">
+    return `<div class="${baseClass}__content" title="${text}">
+      <span class="${baseClass}__content-text">
         ${text}
       </span>
     </div>`;
@@ -68,18 +92,35 @@
           transitionsDelay: initialTransitionDelay,
         },
       },
+      handlers: {
+        mouseenter: ({ dataItem: { id } }) => (hoveredDataItemId.value = id),
+        mouseleave: () => (hoveredDataItemId.value = undefined),
+      },
     });
   });
 
   onBeforeUnmount(() => component.destroy());
 
   watch(params, (value, oldValue) => {
+    const { transitionsDuration } = component.getDefaultConfig();
     const transitionsDelay = Array.isArray(oldValue.data) && !oldValue.data.length ? initialTransitionDelay : 0;
     component.update({
       ...value,
       config: {
         ...value.config,
+        transitionsDuration,
         transitionsDelay,
+      },
+    });
+  });
+
+  watch(hoveredDataItemId, () => {
+    component.update({
+      ...params.value,
+      config: {
+        ...params.value.config,
+        transitionsDuration: mouseTransitionsDuration,
+        transitionsDelay: 0,
       },
     });
   });
@@ -89,6 +130,7 @@
   .treemap {
     &__content {
       display: flex;
+      align-items: flex-start;
       width: 100%;
       height: 100%;
       padding: 5px;
